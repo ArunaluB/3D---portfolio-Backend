dc7b2ffc83f3cb9e385833a31b0717369fff902f
ArunaluB <IT22515612@my.sliit.lk>
2024-07-18 10:20:27 +0530

    fix now

--- app.js
+++ app.js
@@ -3,24 +3,43 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 const emailRoutes = require('./Routers/emailRoutes.js');
+app.use('/api', emailRoutes);
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-app.use(express.static(path.join(__dirname, "public")));
-app.get("/", (re, res) => {
-  res.render("index.html");
-})
+// Catch-all route for handling 403 errors
+app.use((req, res, next) => {
+  const error = new Error('Forbidden');
+  error.status = 403;
+  next(error);
+});
 
+// Error handler middleware
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+      status: error.status
+    }
+  });
+});
 
-app.use('/api', emailRoutes); 
-
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
